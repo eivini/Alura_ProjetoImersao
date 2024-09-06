85f47c556125c2853614cc67954b3d6eb2bc74f9
Halphar <143683210+eivini@users.noreply.github.com>
2024-09-06 21:53:40 +0000

    Projeto Imersão Dev com Gemini

---
+++ app.js
@@ -0,0 +1,82 @@
+for(let dado of projeto){
+    let header = document.getElementById("patrocinio")
+    header.innerHTML += `
+          <section class="patrocinio">
+            <a href="${dado.linkAlura}" target="_blank">Alura</a>
+            <a href="${dado.linkGemini}" target="_blank">Gemini</a>     
+          </section>
+          `
+  }
+  
+  let header = document.getElementById("nomeProjeto");
+  header.innerHTML = `
+        <div>
+          <a href="/" id="link-home"><h1>Halnimes</h1></a>
+        </div>
+        `
+  
+  let section = document.getElementById("buscar-txt")
+  section.innerHTML =`
+        <input type="text" placeholder="Digite o nome do anime" id="campo-busca">
+        <button onclick="buscar()">Buscar</button>
+  `
+  
+  function buscar() {
+    let section = document.getElementById("resultados-busca");
+    let campoBusca = document.getElementById("campo-busca").value;
+    if(!campoBusca.trim()){
+      section.innerHTML = "<p>Você precisa digitar o nome de um anime ou alguma tag referente para conseguir fazer a busca!</p>"
+      return;
+    }
+  
+    campoBusca = campoBusca.toLowerCase();
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+  
+    for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase();
+      descricao = dado.descricao.toLowerCase();
+      tags = dado.tags.toLowerCase();
+      if(titulo.includes(campoBusca) || descricao.includes(campoBusca) || tags.includes(campoBusca)){
+        resultados += `
+        <div class="item-resultado">
+          <h3>
+            <a href="${dado.link}" target="_blank">${dado.titulo}</a>
+          </h3>
+          <p class="descricao-meta">${dado.descricao}</p>
+        </div>
+        `;
+      }
+    }
+  
+    if(!resultados){
+      resultados = "<p>Não conseguimos encontrar nada, poderia tentar digitar novamente?</p>"
+    }
+  
+    section.innerHTML = resultados;
+  }
+  
+  for(let dado of social){
+    let footer = document.getElementById("rodape")
+    footer.innerHTML += `
+          <section class="rodape">
+            <p>Projeto de Imersão Gemini</p>
+            <p>|</p>
+            <p>GitHub:</p>
+            <a href=${dado.linkGit} target="_blank">@eivini</a>
+            <p>|</p>
+            <p>Insta:</p>
+            <a href=${dado.linkInsta} target="_blank">@halphar</a>
+          </section>
+          `
+  }
+  
+  let campoBusca = document.getElementById("campo-busca");
+  
+  campoBusca.addEventListener('keyup', function(event) {
+      if (event.key === 'Enter') {
+          buscar();
+      }
+  });
---
+++ dados.js
@@ -0,0 +1,45 @@
+let dados = [
+    {
+        titulo: "Fullmetal Alchemist: Brotherhood",
+        descricao: "Fullmetal Alchemist: Brotherhood é uma obra-prima da animação japonesa que cativa espectadores de todas as idades. A história acompanha os irmãos Edward e Alphonse Elric em uma jornada épica para recuperar seus corpos após um experimento de alquimia fracassado. A trama, rica em elementos de fantasia, filosofia e drama, explora temas profundos como a perda, o sacrifício, a ética e a busca pela verdade. A animação impecável, a trilha sonora envolvente e os personagens complexos e carismáticos fazem de Brotherhood um anime memorável, que continua a inspirar e emocionar o público anos após sua estreia.",
+        link: "https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood",
+        tags: "fma todos"
+    },
+    {
+        titulo: "Hunter x Hunter (2011)",
+        descricao: "Hunter x Hunter (2011) é uma adaptação anime que cativou fãs ao redor do mundo com sua narrativa envolvente e personagens complexos. A história acompanha Gon Freecss, um jovem determinado a se tornar um Hunter, um caçador profissional que busca tesouros, criaturas e lugares misteriosos. Ao lado de seus amigos, Killua, Leorio e Kurapika, Gon embarca em uma jornada épica repleta de desafios, perigos e descobertas sobre si mesmo e o mundo ao seu redor. A série se destaca por sua animação impecável, arcos narrativos emocionantes e um universo rico em detalhes, explorando temas como amizade, ambição, honra e amadurecimento. Com uma mistura de ação, comédia e drama, Hunter x Hunter se tornou um clássico do gênero shonen, conquistando o público com sua profundidade e qualidade.",
+        link: "https://myanimelist.net/anime/11061/Hunter_x_Hunter_2011",
+        tags: "hxh hxh2021 hunters todos"
+    },
+    {
+        titulo: "Death Note",
+        descricao: "Death Note é um anime psicológico que prende a atenção do público com sua trama intrigante e personagens complexos. A história gira em torno de Light Yagami, um estudante do ensino médio que encontra um caderno sobrenatural com o poder de matar qualquer pessoa cujo nome seja escrito nele. Ao assumir a identidade de Kira, Light embarca em uma cruzada para criar um mundo 'mais justo', eliminando criminosos e pessoas que ele considera indignas de viver. A série explora temas como justiça, moralidade e as consequências de ter um poder absoluto, enquanto Light é perseguido por L, um detetive brilhante que busca desmascarar Kira. Com reviravoltas constantes e diálogos inteligentes, Death Note se tornou um clássico do anime, desafiando os espectadores a questionar suas próprias noções de certo e errado.",
+        link: "https://myanimelist.net/anime/1535/Death_Note",
+        tags: "caderno kira light todos"
+    },
+    {
+        titulo: "Toradora!",
+        descricao: "Toradora! é uma deliciosa comédia romântica que nos apresenta uma história de amor nada convencional. A trama gira em torno de Ryuuji Takasu, um jovem com uma aparência intimidadora, mas um coração gentil, e Taiga Aisaka, uma garota pequena e fofa, mas com um temperamento explosivo. Quando esses dois opostos se unem para ajudar um ao outro a conquistar seus amores, uma série de situações hilárias e emocionantes se desenrolam. Com personagens cativantes, diálogos inteligentes e uma pitada de drama, Toradora! nos mostra que o amor pode florescer nos lugares mais inesperados, desafiando os estereótipos e nos ensinando sobre amizade, aceitação e a importância de sermos nós mesmos.",
+        link: "https://myanimelist.net/anime/4224/Toradora",
+        tags: "taiga ryuuji ryuji todos"
+    },
+    {
+        titulo: "One Piece",
+        descricao: "One Piece é um anime e mangá japonês que cativou milhões de fãs ao redor do mundo. Criado por Eiichiro Oda, a história acompanha Monkey D. Luffy, um jovem pirata que sonha em encontrar o tesouro lendário One Piece e se tornar o Rei dos Piratas. Junto de sua excêntrica tripulação, o Chapéu de Palha, Luffy navega pelos vastos mares do Grand Line, enfrentando desafios perigosos, desvendando mistérios ancestrais e forjando amizades duradouras. Com uma mistura única de ação, comédia, drama e fantasia, One Piece oferece uma jornada épica que celebra a amizade, a liberdade e a busca por seus sonhos.",
+        link: "https://myanimelist.net/anime/21/One_Piece",
+        tags: "luffy d. pirata todos"
+
+    }
+];
+let social = [
+    {
+        linkGit: "https://github.com/eivini",
+        linkInsta: "https://www.instagram.com/halphar/"
+    }
+]
+let projeto = [
+    {
+        linkAlura: "https://www.alura.com.br/",
+        linkGemini: "https://gemini.google.com/"
+    }
+]
